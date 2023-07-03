a7e7814e9ab241dad416051cdf9b62e2d8fa0769
GiulioAtMaterea <giuliomilani@materea.industries>
2023-07-03 15:07:26 +0200

    test

--- app/routers/paths.py
+++ app/routers/paths.py
@@ -22,11 +22,11 @@ def get_path(method: paths.PathMethod, points: int, index = None):
     image = paths.plot_points(paths.get_path(method, points), index)
     return Response(image, media_type="image/png")
 
-@router.get("/adv_{method}",  response_model=list[paths.CartesianPoint3D])
+@router.get("/adv/{method}",  response_model=list[paths.CartesianPoint3D])
 async def get_adv_path(method: paths.PathMethod, num_points:int, num_rings:int, max_a:int, min_a:int):
         return paths.adv_get_path(method, num_points, num_rings, max_a, min_a)
 
-@router.get("/adv_{method}/preview")
+@router.get("/adv/{method}/preview")
 def adv_get_path(method: paths.PathMethod, num_points:int, num_rings:int, max_a:int, min_a:int, index = None):
     image = paths.plot_points(paths.adv_get_path(method,num_points, num_rings, max_a, min_a), index)
     return Response(image, media_type="image/png")
